756a9120808c3d1d88003a3f1ce9cad44d3b194d
Mykola Krasko <mikokrsk@gmail.com>
2023-07-04 13:16:00 +0300

    Added Lives UI
    + Lives Text
    + Lives value

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,26 +8,35 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
     public GameObject titleScreen;
     public bool isGameActive;
+    public int lives = 3;
     private int score;
     private float spawnRate = 1f;
     // Start is called before the first frame update
     void Start()
     {
-
+        scoreText.text = "Score : " + score;
+        livesText.text = "Lives : " + lives;
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
 
     }
-
+    public void UpdateLives(int livesToAdd)
+    {
+        lives += livesToAdd;
+        livesText.text = "Lives : " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
